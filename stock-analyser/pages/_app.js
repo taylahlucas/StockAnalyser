286a6e179fe6bc523504949be893f1eb62dd2e3d
taylahlucas <taylahlucas.dev@gmail.com>
2022-07-01 11:00:07 +1000

    added redux and observables

--- stock-analyser/pages/_app.js
+++ stock-analyser/pages/_app.js
@@ -1,15 +1,22 @@
 import '../styles/global.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import { useEffect } from 'react'
+import { Provider } from 'react-redux'
+import { useStore, wrapper } from '../redux/store';
 
 
-function MyApp({ Component, pageProps }) {
+function StockAnalyserApp({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState)
   // Import bootsrap here to work with components that use Javascript
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
+    import('bootstrap/dist/js/bootstrap')
   }, []);
 
-  return <Component {...pageProps} />
+  return (    
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+    )
 }
 
-export default MyApp
+export default wrapper.withRedux(StockAnalyserApp)
---
+++ stock-analyser/redux/store.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
+import { createWrapper } from "next-redux-wrapper"
+import company_reducer from "./reducers"
+
+// Initial states
+const initialState = {}
+
+// Middleware
+const middleware = [thunk]
+
+// Creating store
+export const store = createStore(
+    company_reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+// Assignment store to next wrappper
+const makeStore = () => store
+
+export const wrapper = createWrapper(makStore)
