d13ce261a9dfe7255396c0717d641e8cfb6e08ce
taylahlucas <taylahlucas.dev@gmail.com>
2022-08-05 15:35:56 +1000

    fixed folder structure

--- stock-analyser/src/common/components/side_bar/company_list.js
+++ stock-analyser/src/common/components/side_bar/company_list.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
-import mockCompanies from '../../data/asx_mock_company_data'
+import mockCompanies from '../../../data/asx_mock_company_data'
 import { List } from '@material-ui/core'
 
 const propTypes = {
