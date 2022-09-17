7a3cf40378bd14da5d82355c3552fac84a0bd794
taylahlucas <taylahlucas.dev@gmail.com>
2022-09-17 17:44:29 +1000

    fixed themes

--- stock-analyser/src/styles/mui_themes.js
+++ stock-analyser/src/styles/mui_themes.js
@@ -1,62 +1,70 @@
 import { createTheme } from '@mui/material'
 
-export const baseTheme = {
+export const baseTheme = createTheme({
     palette: {
         background: {
             default: '#14131A'
         }
     },
-    overrides: {
+    components: {
         MuiButton: {
-            root: {
-                width: 250,
-                height: 40,
-                background: '#2F2A36',
-                borderRadius: 2,
-                zIndex: 1,
-                '& $label': {
+            styleOverrides: {
+                root: {
+                    width: 250,
+                    height: 40,
+                    background: '#2F2A36',
+                    borderRadius: 2,
+                    zIndex: 1,
+                },
+                text: {
                     fontFamily: 'Avenir Next',
                     fontSize: 14,
                     color: '#E9E9E9',
                     textTransform: 'uppercase'
                 }
-            }
+            }            
         },
         MuiListItem: {
-            root: {
-                width: 230,
-                minHeight: 30,
-                marginLeft: 20,
-                background: '#727276',
-                // Label properties
-                fontFamily: 'Avenir Next',
-                fontSize: 10,
-                color: '#E9E9E9',
-                textTransform: 'uppercase',
-                justifyContent: 'left'
+            styleOverrides: {
+                root: {
+                    width: 230,
+                    minHeight: 30,
+                    marginLeft: 20,
+                    background: '#727276',
+                    // Label properties
+                    fontFamily: 'Avenir Next',
+                    fontSize: 10,
+                    color: '#E9E9E9',
+                    textTransform: 'uppercase',
+                    justifyContent: 'left'
+                }
             }
         },
         MuiTextField: {
-            root: {
-                width: '265px', 
-                height: '35px', 
-                backgroundColor: 'transparent'
+            styleOverrides: {
+                root: {
+                    width: '265px', 
+                    height: '35px', 
+                    backgroundColor: 'transparent'
+                }
             }
         },
         MuiInput: {
-            'input': {
-                paddingLeft: '15px',
-                fontFamily: 'Avenir Next',
-                color: '#E9E9E9',
-                opacity: 0.6,
-                '&::placeholder': {
-                    fontSize: 16,
-                    color: '#707070'
+            styleOverrides: {
+                'input': {
+                    paddingLeft: '15px',
+                    fontFamily: 'Avenir Next',
+                    color: '#E9E9E9',
+                    opacity: 0.6,
+                    '&::placeholder': {
+                        fontSize: 16,
+                        color: '#707070'
+                    }
                 }
             }
         }
-    }
-}
+    },
+})
 
 export const subItemTheme = createTheme({
     ...baseTheme,
