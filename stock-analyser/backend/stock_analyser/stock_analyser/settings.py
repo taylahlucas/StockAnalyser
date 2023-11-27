7307b2b4fa8a08dc0807f865d15f57225dc2b8c5
Taylah Lucas <Taylah.lucas@hunterindustries.com>
2023-11-27 15:52:20 +0000

    Security updates

--- stock-analyser/backend/stock_analyser/stock_analyser/settings.py
+++ stock-analyser/backend/stock_analyser/stock_analyser/settings.py
@@ -11,16 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ktmx_w*4a*-nb3ms^*i0_%7nj+2*mb(#g7$dickrjy_&6h+lc6'
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,13 +81,6 @@ WSGI_APPLICATION = 'stock_analyser.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
